602553caaacadccb17d057a3d783fc3105e561b0
CKodes <ckodes@outlook.com>
2023-10-31 17:26:20 +0800

    feat: Created Navbar and Footer sgds components
    For testing purposes to visualize article page UI.

---
+++ src/sgdsComponents/Footer.jsx
@@ -0,0 +1,25 @@
+import { Footer } from "@govtechsg/sgds-react/Footer";
+
+function FooterSgds() {
+  return (
+    <Footer>
+      <Footer.Top>
+        <Footer.Top.Header headerTitle="Imaginary Government Design System">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum illo
+          delectus laborum libero id ratione quibusdam tempora assumenda quas,
+          pariatur cum minus, aliquid molestiae et nisi dolorem vitae molestias!
+          Voluptate commodi aliquid iusto sequi sit eligendi, quod numquam nihil
+          consectetur eaque error earum laudantium! Temporibus accusamus
+          pariatur quod totam quia.
+        </Footer.Top.Header>
+      </Footer.Top>
+      <Footer.Bottom>
+        <Footer.Bottom.Copyrights>
+          © 2022 Government of My Imagination. Last Updated 08 Feb 2022
+        </Footer.Bottom.Copyrights>
+      </Footer.Bottom>
+    </Footer>
+  );
+}
+
+export default FooterSgds;
