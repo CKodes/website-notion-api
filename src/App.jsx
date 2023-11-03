8aec966b2f3103c05a5adb0a1c647fdec4cddd44
CKodes <ckodes@outlook.com>
2023-11-03 16:45:36 +0800

    refactor: imported and updated components for testing
    - Added imports for new components.
    
    Removed imports for unnecessary comoponents.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import Message from './Message.jsx';
-import NavbarSgds from './sgdsComponents/Navbar.jsx';
-import FooterSgds from './sgdsComponents/Footer.jsx';
-import ArticlePage from './pages/ArticlePage.jsx';
-import '@govtechsg/sgds/css/sgds.css';
-import './App.css';
+import GridArticlePage from "./pages/GridArticlePage.jsx";
+import DetailedPageSideNavExtended from "./sgdsTemplates/DetailedPageSideNavExtended.jsx";
+import NavbarSgds from "./sgdsComponents/Navbar.jsx";
+import FooterSgds from "./sgdsComponents/Footer.jsx";
+import ArticlePage from "./pages/ArticlePage.jsx";
+import "@govtechsg/sgds/css/sgds.css";
+import "./App.css";
 
 function App() {
   return (
     <>
       <NavbarSgds />
-      <ArticlePage />
+      {/* <ArticlePage /> */}
+      <GridArticlePage />
+      {/* <DetailedPageSideNavExtended /> */}
       <FooterSgds />
     </>
   );
