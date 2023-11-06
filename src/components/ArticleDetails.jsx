3a718c69f9c70ef608ecbb3b20e4097137784806
CKodes <ckodes@outlook.com>
2023-11-06 12:31:20 +0800

    refactor: added imports and updated css classes
    Added imports of svg icons used for article date, written by and reading time
    
    Updated css classes to match bootstrap classes

--- src/components/ArticleDetails.jsx
+++ src/components/ArticleDetails.jsx
@@ -1,42 +1,40 @@
+import CalendarIcon from "../assets/calendar.svg";
+import NotesIcon from "../assets/notes.svg";
+import PersonIcon from "../assets/person.svg";
+
 function ArticleDetails() {
   return (
-    <section className="p-1rem" aria-label="article details">
+    <section className="" aria-label="article details">
       <h1 className="m-0">Blog Article Title</h1>
-      <div className="flex-column p-top-1rem gap-03rem">
-        <div className="flex-row gap-03rem">
+      <div className="d-flex flex-column pt-2 pb-5 gap-2">
+        <div className="d-flex flex-row gap-2">
           <div>
-            <span
-              className="sgds-icon sgds-icon-calendar"
-              role="img"
-              aria-label="Article Date"
-            ></span>
+            <span aria-label="Article date">
+              <img src={CalendarIcon} role="img"></img>
+            </span>
           </div>
           <div>1st January 2024</div>
         </div>
-        <div className="flex-row gap-03rem">
+        <div className="d-flex flex-row gap-2">
           <div>
-            <span
-              className="sgds-icon sgds-icon-person"
-              role="img"
-              aria-label="Written by"
-            ></span>
+            <span aria-label="Written by">
+              <img src={PersonIcon} role="img"></img>
+            </span>
           </div>
           <div>Riley Wee</div>
         </div>
-        <div className="flex-row gap-03rem">
+        <div className="d-flex flex-row gap-2">
           <div>
-            <span
-              className="sgds-icon sgds-icon-text-left"
-              role="img"
-              aria-label="Reading time"
-            ></span>
+            <span aria-label="Reading Time">
+              <img src={NotesIcon} role="img"></img>
+            </span>
           </div>
           <div>5 Minutes</div>
         </div>
-        <div className="flex-row gap-3">
-          <span className="tag-items">Git</span>
-          <span className="tag-items">React</span>
-          <span className="tag-items">Typescript</span>
+        <div className="d-flex flex-row gap-3">
+          <span className="tag-items px-2">git commits</span>
+          <span className="tag-items px-2">react</span>
+          <span className="tag-items px-2">typescript</span>
         </div>
       </div>
     </section>
