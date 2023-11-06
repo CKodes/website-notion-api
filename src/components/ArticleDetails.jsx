6c39f1d16e5e055b525cfb5fde3f5b5845d8a0e9
CKodes <ckodes@outlook.com>
2023-11-06 12:07:54 +0800

    feat: added tag-items to ArticleDetails component
    tag-items will represent topics covered in the article

--- src/components/ArticleDetails.jsx
+++ src/components/ArticleDetails.jsx
@@ -1,37 +1,42 @@
 function ArticleDetails() {
   return (
-    <section className='p-1rem' aria-label='article details'>
-      <h1 className='m-0'>Blog Article Title</h1>
-      <div className='flex-column p-top-1rem gap-03rem'>
-        <div className='flex-row gap-03rem'>
+    <section className="p-1rem" aria-label="article details">
+      <h1 className="m-0">Blog Article Title</h1>
+      <div className="flex-column p-top-1rem gap-03rem">
+        <div className="flex-row gap-03rem">
           <div>
             <span
-              className='sgds-icon sgds-icon-calendar'
-              role='img'
-              aria-label='Article Date'
+              className="sgds-icon sgds-icon-calendar"
+              role="img"
+              aria-label="Article Date"
             ></span>
           </div>
           <div>1st January 2024</div>
         </div>
-        <div className='flex-row gap-03rem'>
+        <div className="flex-row gap-03rem">
           <div>
             <span
-              className='sgds-icon sgds-icon-person'
-              role='img'
-              aria-label='Written by'
+              className="sgds-icon sgds-icon-person"
+              role="img"
+              aria-label="Written by"
             ></span>
           </div>
           <div>Riley Wee</div>
         </div>
-        <div className='flex-row gap-03rem'>
+        <div className="flex-row gap-03rem">
           <div>
             <span
-              className='sgds-icon sgds-icon-text-left'
-              role='img'
-              aria-label='Reading time'
+              className="sgds-icon sgds-icon-text-left"
+              role="img"
+              aria-label="Reading time"
             ></span>
           </div>
           <div>5 Minutes</div>
+        </div>
+        <div className="flex-row gap-3">
+          <span className="tag-items">Git</span>
+          <span className="tag-items">React</span>
+          <span className="tag-items">Typescript</span>
         </div>
       </div>
     </section>
