7b3701bc60cbf5d27d4be77fc48ab89b042de9e8
CKodes <ckodes@outlook.com>
2023-11-09 18:06:38 +0800

    feat: created ArticlesLibraryPage
    This page will hold a collection of ArticleCard representing all articles.

---
+++ src/pages/ArticlesLibraryPage.jsx
@@ -0,0 +1,16 @@
+import ArticleCardSgds from "../sgdsComponents/ArticleCard";
+
+function ArticlesLibraryPage() {
+  return (
+    <main className="article-card-container">
+      <ArticleCardSgds />
+      <ArticleCardSgds />
+      <ArticleCardSgds />
+      <ArticleCardSgds />
+      <ArticleCardSgds />
+      <ArticleCardSgds />
+    </main>
+  );
+}
+
+export default ArticlesLibraryPage;
